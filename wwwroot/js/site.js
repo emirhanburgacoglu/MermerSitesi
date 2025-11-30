72243e6f80867e3b8e33e9e25d75dffdc36b0893
emirhanburgacoglu <emirhanburgacoglu@gmail.com>
2025-12-01 01:39:59 +0300

    Navbar: Koleksiyon menüsüne özel ikon eklendi ve hover efekti son hale getirildi

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,21 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+// Slider Numara Güncelleme
+var myCarousel = document.getElementById('heroCarousel')
+var currentSlideElement = document.getElementById('currentSlide');
+var progressBar = document.getElementById('progressBar');
+
+if (myCarousel) {
+    myCarousel.addEventListener('slide.bs.carousel', function (e) {
+        // e.to, gidilecek slaytın indexidir (0'dan başlar)
+        var slideIndex = e.to + 1; // 1'den başlatmak için +1 ekliyoruz
+
+        // Numarayı güncelle (Başına 0 koyarak)
+        currentSlideElement.innerText = "0" + slideIndex + ".";
+
+        // Progress Bar'ı güncelle (4 slayt var varsayıyoruz)
+        var progressPercent = (slideIndex / 4) * 100;
+        progressBar.style.width = progressPercent + "%";
+    })
+}
